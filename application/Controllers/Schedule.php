8111ba485b804566761c5d977577f7ffe1bbc466
HaoLiang <haoliang@outlook.jp>
2017-09-04 11:50:21 +0800

    1、修改时区 2、修改定时执行日期

--- application/Controllers/Schedule.php
+++ application/Controllers/Schedule.php
@@ -15,13 +15,13 @@ class Schedule extends Controller
 		$scheduler = new Scheduler();
 		// 定时获取黄金数据接口 Start
 		$scheduler->php($this->script, '/usr/local/bin/php', ['finance' => 'gold'])
-				  ->at('*/15 0-2,9-11,20-24 * * 1-5');
+				  ->at('*/15 0-2,9-11,20-23 * * 1-5');
 		$scheduler->php($this->script, '/usr/local/bin/php', ['finance' => 'gold'])
-				  ->at('30,45, 13 * * 1-5');
+				  ->at('30,45 13 * * 1-5');
 		$scheduler->php($this->script, '/usr/local/bin/php', ['finance' => 'gold'])
-				  ->at('*/15, 14 * * 1-5');
+				  ->at('*/15 14 * * 1-5');
 		$scheduler->php($this->script, '/usr/local/bin/php', ['finance' => 'gold'])
-				  ->at('0,15,30, 15 * * 1-5');
+				  ->at('0,15,30 15 * * 1-5');
 		// 定时获取黄金数据接口 End
 
 
